aef9363f7d6c3841ed80e4f2e5f561aa8bf87214
Jonathan Chan <tmxistz@gmail.com>
2020-07-17 18:22:35 +0900

    added fileanalyse path

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-'use strict';
-
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // require and use "multer"...
+const multer = require('multer');
+const upload = multer();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    const { originalname, mimetype, size } = req.file;
+
+    res.json({ name: originalname, type: mimetype, size: size });
+  } catch (error) {
+    console.error(error);
+    res.json({ message: error.message });
+  }
 });
 
 app.listen(process.env.PORT || 3000, function () {
